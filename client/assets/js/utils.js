35d702f889506e7d5080b4b727ce74a5ef800723
Rob <robertdacorte@gmail.com>
2023-09-08 08:23:18 +0200

    Solution files

---
+++ client/assets/js/utils.js
@@ -0,0 +1,31 @@
+export const formatCurrency = (value) => {
+	return new Intl.NumberFormat('es-ES', {
+		style: 'currency',
+		currency: 'EUR'
+	}).format(value);
+}
+
+export const formatDateShort = (value) => {
+	return new Intl.DateTimeFormat('es-ES', {
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric'
+	}).format(new Date(value));
+}
+
+export const accountMask = (value) => {
+	const mask = '**** **** **** **** $5';
+	return value.replace(/(\w{2}\d{2})(\d{4})(\d{4})(\d{4})(\d{4})/, mask);
+}
+
+export const translateToSpanish = (value) => {
+	const translations = {
+		'checking': 'Cuenta nómina',
+		'savings': 'Cuenta de ahorro',
+	}
+	return translations[value] || value;
+}
+
+export const accountFormat = (value) => {
+	return value.replace(/(\w{2}\d{2})(\d{4})(\d{4})(\d{4})(\d{4})/, '$1 $2 $3 $4 $5');
+}
